6d223a39b4cd20db573665eb1c3aff2da41f3df6
Voyagerr_77 <whydowefall91@gmail.com>
2024-02-10 17:32:30 +0200

    add file

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿//Задача 1: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. Использовать рекурсию, не использовать циклы.
+
+void PrintInterval(int m, int n)
+		{
+			int start = m;
+			int end = n;
+			if(m > n)
+			{
+				start = n;
+				end = m;
+			}
+			for(int i = start; i <= end; i++)
+			{
+				Console.Write($"{i} ");
+			}
+		}
+		PrintInterval(140, 50);
+		
+		Console.WriteLine();
+
+
+//Задача 2: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
+
+		int AkkermanFunc(int m, int n)
+		{
+			if(m == 0)
+			{
+				return n + 1;
+			}
+			if(m > 0 && n == 0)
+			{
+				return AkkermanFunc(m - 1, 1);
+			}
+			return AkkermanFunc(m - 1, AkkermanFunc(m, n - 1));
+		}
+		
+		Console.WriteLine(AkkermanFunc(1,12));
+
+
+//Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
