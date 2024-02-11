b940e02d93d428a7c699084b11c0a988c7da63ce
Voyagerr_77 <whydowefall91@gmail.com>
2024-02-11 17:12:35 +0200

    add task3

--- Program.cs
+++ Program.cs
@@ -1,22 +1,29 @@
 ﻿//Задача 1: Задайте значения M и N. Напишите программу, которая выведет все натуральные числа в промежутке от M до N. Использовать рекурсию, не использовать циклы.
 
 void PrintInterval(int m, int n)
-		{
-			int start = m;
-			int end = n;
-			if(m > n)
-			{
-				start = n;
-				end = m;
-			}
-			for(int i = start; i <= end; i++)
-			{
-				Console.Write($"{i} ");
-			}
-		}
-		PrintInterval(140, 50);
-		
-		Console.WriteLine();
+{
+  int start = m;
+  int end = n;
+  if(m > n)
+  {
+    start = n;
+    end = m;
+  }
+
+  Console.Write($"{start} ");
+
+  start = start + 1;
+
+  if (start > end) {
+    return;
+  } else {  
+    PrintInterval(start, end);
+  }
+}
+
+PrintInterval(140, 50);
+
+Console.WriteLine();
 
 
 //Задача 2: Напишите программу вычисления функции Аккермана с помощью рекурсии. Даны два неотрицательных числа m и n.
@@ -38,3 +45,22 @@ void PrintInterval(int m, int n)
 
 
 //Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+
+
+	void printReversed(int[] arr, int? optionalIndex = null) {
+	
+	
+		int i = optionalIndex ?? arr.Length - 1;
+		
+		if (i < 0) {
+			return;
+		}
+
+		Console.WriteLine(arr[i]);
+		
+		printReversed(arr, i - 1);
+		}
+		int[] array = {1, 2, 3, 4, 5, 6};
+		
+	printReversed(array);	
+	
